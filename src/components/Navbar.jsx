957160486d97bf47b7e763753ffdb6fa81838e1b
MirasLearnsCoding <136673406+MirasLearnsCoding@users.noreply.github.com>
2024-10-21 19:36:16 +0500

    basic stuff

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <h1>Welcome Naruto fan!</h1>
+        <div className="navbar__links">
+            <div>Tasks</div>
+            <div>About</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
